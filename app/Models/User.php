a3bcafe9f7be834ccdd959b730d1a11d9bef7f6b
JosuePacheco <al222010785@gmail.com>
2022-04-06 16:28:50 -0500

    Modelo Users

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,15 +31,32 @@ class User extends Authenticatable
         
     ];
 
+    public static function reglasValidacion() {
+        // https://laravel.com/docs/9.x/validation#available-validation-rules
+        return [
+            'name' => 'required|string|min:10|max:50'
+            , 'primer_apellido' => 'required|string|min:3|max:100'
+            , 'segundo_apellido' => 'required|string|min:3|max:100'
+            , 'fecha_nacimiento' => 'required|string|min:9|max:11'
+            , 'sexo' => 'required|in:' . implode(',', self::opcionesSexo())
+            , 'rol' => 'required|in:' . implode(',', self::opcionesRol())
+            , 'estatus' => 'required|in:' . implode(',', self::opcionesEstatus())
+            , 'email' => 'required|string|min:10|max:30'
+            , 'password' => 'required|string|min:6|max:30'
+        ];
+    }
+
     public static function etiquetas() {
         return [
         'name' => 'name',
         'primer_apellido' => 'primer_apellido',
         'segundo_apellido' => 'segundo_apellido',
         'fecha_nacimiento' => 'fecha_nacimiento',
-        'sexo',
-        'email',
-        'password'
+        'sexo' => 'sexo',
+        'rol' => 'rol',
+        'estatus' => 'estatus',
+        'email' => 'email',
+        'password' => 'password'
         ];
     }
 
@@ -61,4 +78,27 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function opcionesSexo() {
+        return [
+            'femenino' => 'femenino'
+            , 'masculino' => 'masculino'
+            , 'prefiero no decirlo' => 'prefiero no decirlo'
+        ];
+    }
+
+    public static function opcionesRol() {
+        return [
+            'Administrador' => 'Administrador'
+            , 'Cliente' => 'Cliente'
+            , 'Proveedor' => 'Proveedor'
+        ];
+    }
+
+    public static function opcionesEstatus() {
+        return [
+            'activo' => 'activo'
+            , 'inactivo' => 'inactivo'
+        ];
+    }
 }
